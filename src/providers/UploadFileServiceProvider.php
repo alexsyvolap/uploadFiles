04996159148df5054a370109c5f96d57a74f9b4e
Alex Syvolap <snikersmc123@gmail.com>
2020-11-06 12:39:56 +0200

    change path to published

--- src/providers/UploadFileServiceProvider.php
+++ src/providers/UploadFileServiceProvider.php
@@ -23,7 +23,7 @@ class UploadFileServiceProvider extends ServiceProvider
     public function publishConfig()
     {
         $this->publishes([
-            __DIR__ . '/config/upload_files.php' => config_path('upload_files.php'),
+            __DIR__ . '/../config/upload_files.php' => config_path('upload_files.php'),
         ], 'san4o101-config');
     }
 
@@ -32,21 +32,21 @@ class UploadFileServiceProvider extends ServiceProvider
         $this->loadMigrationsFrom(__DIR__ . '/migrations');
 
         $this->publishes([
-            __DIR__ . '/migrations' => database_path('migrations/san4o101'),
+            __DIR__ . '/../migrations' => database_path('migrations/san4o101'),
         ], 'san4o101-migrations');
     }
 
     public function publishService()
     {
         $this->publishes([
-            __DIR__ . '/Services/UploadFileService.php' => app_path('Services/san4o101/UploadFileService.php'),
+            __DIR__ . '/../Services/UploadFileService.php' => app_path('Services/san4o101/UploadFileService.php'),
         ], 'san4o101-service');
     }
 
     public function publishModel()
     {
         $this->publishes([
-            __DIR__ . '/Models/SFile.php' => app_path('Models/san4o101/UploadFileService.php'),
+            __DIR__ . '/../Models/SFile.php' => app_path('Models/san4o101/UploadFileService.php'),
         ], 'san4o101-migrations');
     }
 }
